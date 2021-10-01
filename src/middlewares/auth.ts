f04db7cb6617fa6fa738f1fceb9981af01ae73b3
GIAVINH2010 <vinhtranvu2010@gmail.com>
2021-10-01 13:38:08 +0700

    fix: 🐛 handle missing token

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -4,11 +4,11 @@ import jwt from "jsonwebtoken";
 const jwtSecretKey = process.env.JWT_SECRET_KEY || "tuheumapdit";
 
 export const checkJwt = (req: Request, res: Response, next: NextFunction) => {
-  //Get the jwt token from the head
-  const token = <string>req.headers["authorization"].split(" ")[1];
-
   //Try to validate the token and get data
   try {
+    //Get the jwt token from the head
+    const token = <string>req.headers["authorization"].split(" ")[1];
+
     const decoded = <any>jwt.verify(token, jwtSecretKey);
     res.locals.jwtPayload = decoded;
   } catch (error) {
